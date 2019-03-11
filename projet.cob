dc78aa84a7cc6834a38b7a763af06c25e7cf20f6
Quentin Billy <quentinbilly@Quentins-MacBook-Pro.local>
2019-03-11 14:50:33 +0100

    Ajout des deux fonctions. Le code compile !

--- projet.cob
+++ projet.cob
@@ -1,135 +1,142 @@
 IDENTIFICATION DIVISION.
-
         PROGRAM-ID. Projet.
 
 ENVIRONMENT DIVISION.
-
-        INPUT-OUTPUT SECTION.
+INPUT-OUTPUT SECTION.
         FILE-CONTROL.
-        SELECT Femploye ASSIGN TO "employes.dat"
-        ORGANIZATION INDEXED
-        RECORD KEY fe_idEmploye
-        FILE STATUS IS FcER.
+           SELECT Femploye ASSIGN TO "employes.dat"
+           ORGANIZATION INDEXED
+           RECORD KEY fe_idEmploye
+           FILE STATUS IS FcER.
 
-        SELECT Fproduit ASSIGN TO "produits.dat"
-        ORGANIZATION INDEXED
-        RECORD KEY fp_idProduit
-        FILE STATUS IS FcPR.
+           SELECT Fproduit ASSIGN TO "produits.dat"
+           ORGANIZATION INDEXED
+           RECORD KEY fp_idProduit
+           FILE STATUS IS FcPR.
 
-        SELECT Fclient ASSIGN TO "clients.dat"
-        ORGANIZATION INDEXED
-        RECORD KEY fc_idClient
-        FILE STATUS IS FcCR.
+           SELECT Fclient ASSIGN TO "clients.dat"
+           ORGANIZATION INDEXED
+           RECORD KEY fc_idClient
+           FILE STATUS IS FcCR.
 
-        SELECT Fachat ASSIGN TO "achats.dat"
-        ORGANIZATION INDEXED
-        RECORD KEY fa_idAchat
-        ALTERNATE RECORD KEY fa_idProduit WITH DUPLICATES
-        ALTERNATE RECORD KEY fa_dateAch WITH DUPLICATES
-        ALTERNATE RECORD KEY fa_idEmploye WITH DUPLICATES
-        ALTERNATE RECORD KEY fa_idClient WITH DUPLICATES
-        FILE STATUS IS FcAR.
+           SELECT Fachat ASSIGN TO "achats.dat"
+           ORGANIZATION INDEXED
+           RECORD KEY fa_idAchat
+           ALTERNATE RECORD KEY fa_idProduit WITH DUPLICATES
+           ALTERNATE RECORD KEY fa_dateAch WITH DUPLICATES
+           ALTERNATE RECORD KEY fa_idEmploye WITH DUPLICATES
+           ALTERNATE RECORD KEY fa_idClient WITH DUPLICATES
+           FILE STATUS IS FcAR.
 
-        SELECT Fhistorique ASSIGN TO "historiques.dat"
-        ORGANIZATION INDEXED
-        RECORD KEY fh_cle
-        FILE STATUS IS FcHR.
+           SELECT Fhistorique ASSIGN TO "historiques.dat"
+           ORGANIZATION INDEXED
+           RECORD KEY fh_cle
+           FILE STATUS IS FcHR.
 
-        SELECT Fidentifiant ASSIGN TO "identifiants.dat"
-        ORGANIZATION SEQUENTIAL
-        FILE STATUS IS FcIR.
+           SELECT Fidentifiant ASSIGN TO "identifiants.dat"
+           ORGANIZATION SEQUENTIAL
+           FILE STATUS IS FcIR.
 
 DATA DIVISION.
-
         FILE SECTION.
-        FD Femploye.
-        01 employeTemp.
-                02 fe_idEmploye PIC 9(4).
-                02 fe_nom PIC A(30).
-                02 fe_prenom PIC A(30).
-                02 fe_salaire PIC 9(10).
-                02 fe_RIB PIC A(36).
-                02 fe_adresse PIC A(30).
-                02 fe_nbVentes PIC 9(36).
+           FD Femploye.
+              01 employeTemp.
+                      02 fe_idEmploye PIC 9(4).
+                      02 fe_nom PIC A(30).
+                      02 fe_prenom PIC A(30).
+                      02 fe_salaire PIC 9(5)V9(2).
+                      02 fe_rib PIC A(23).
+                      02 fe_adresse PIC A(30).
+                      02 fe_nbVente PIC 9(13).
 
-        FD Fproduit.
-        01 produitTemp.
-                02 fp_idProduit PIC 9(4).
-                02 fp_nom PIC A(30).
-                02 fp_prix PIC 9(10).
-                02 fp_quantite PIC 9(36).
+           FD Fproduit.
+              01 produitTemp.
+                      02 fp_idProduit PIC 9(4).
+                      02 fp_nom PIC A(30).
+                      02 fp_prix PIC 9(5).
+                      02 fp_quantite PIC 9(13).
 
-        FD Fclient.
-        01 clientTemp.
-                02 fc_idClient PIC 9(4).
-                02 fc_nom PIC A(30).
-                02 fc_prenom PIC A(30).
-                02 fc_dateNaiss.
-                	03 fc_annee PIC 9(4).
-                        03 fc_mois PIC 9(2).
-                        03 fc_jour PIC 9(2).
-                02 fc_codePost PIC 9(5).
-                02 fc_nbArtAch PIC 9(36).
+           FD Fclient.
+              01 clientTemp.
+                      02 fc_idClient PIC 9(4).
+                      02 fc_nom PIC A(30).
+                      02 fc_prenom PIC A(30).
+                      02 fc_dateNaiss.
+                      	03 fc_annee PIC 9(4).
+                              03 fc_mois PIC 9(2).
+                              03 fc_jour PIC 9(2).
+                      02 fc_codePost PIC 9(5).
+                      02 fc_nbArtAch PIC 9(13).
 
-        FD Fachat.
-        01 achatTemp.
-                02 fa_idAchat PIC 9(4).
-                02 fa_idProduit PIC 9(4).
-                02 fa_dateAch.
-                	03 fa_annee PIC 9(4).
-                        03 fa_mois PIC 9(2).
-                        03 fa_jour PIC 9(2).
-                02 fa_idEmploye PIC 9(4).
-                02 fa_idClient PIC 9(4).
+           FD Fachat.
+              01 achatTemp.
+                      02 fa_idAchat PIC 9(4).
+                      02 fa_idProduit PIC 9(4).
+                      02 fa_dateAch.
+                      	03 fa_annee PIC 9(4).
+                              03 fa_mois PIC 9(2).
+                              03 fa_jour PIC 9(2).
+                      02 fa_idEmploye PIC 9(4).
+                      02 fa_idClient PIC 9(4).
 
-        FD Fidentifiant.
-        01 identTemp.
-                02 fi_idEmploye PIC 9(4).
-                02 fi_idProduit PIC 9(4).
-                02 fi_idClient PIC 9(4).
-                02 fi_idAchat PIC 9(4).
+           FD Fidentifiant.
+              01 identTemp.
+                      02 fi_idEmploye PIC 9(4).
+                      02 fi_idProduit PIC 9(4).
+                      02 fi_idClient PIC 9(4).
+                      02 fi_idAchat PIC 9(4).
 
-        FD Fhistorique.
-        01 histoTemp.
-                02 fh_cle.
-                	03 fh_annee PIC 9(4).
-                        03 fh_mois PIC 9(2).
-                        03 fh_idArticle PIC 9(4).
-                02 fh_nbAricleVendu PIC 9(36).
+           FD Fhistorique.
+              01 histoTemp.
+                      02 fh_cle.
+                      	03 fh_annee PIC 9(4).
+                              03 fh_mois PIC 9(2).
+                              03 fh_idArticle PIC 9(4).
+                      02 fh_nbAricleVendu PIC 9(13).
 
 
-       WORKING-STORAGE SECTION.
-       77 FcER PIC 9(2).
-       77 FcPR PIC 9(2).
-       77 FcCR PIC 9(2).
-       77 FcAR PIC 9(2).
-       77 FcIR PIC 9(2).
-       77 FcHR PIC 9(2).
+        WORKING-STORAGE SECTION.
+          77 FcER PIC 9(2).
+          77 FcPR PIC 9(2).
+          77 FcCR PIC 9(2).
+          77 FcAR PIC 9(2).
+          77 FcIR PIC 9(2).
+          77 FcHR PIC 9(2).
+          77 pe   PIC 9(1).
 
 PROCEDURE DIVISION.
-
+            *>ICI CORPS DU PROGRAMME
 STOP RUN.
-        ajout_produit.
-            OPEN EXTEND Fproduit.
 
-            PERFORM WITH TEST AFTER UNTIL produitTemp.fp_idProduit > 0
-                DISPLAY "Saisir l'identifiant du produit : "
-                WRITE produitTemp.fp_idProduit
-            END-PERFORM
+recherche_produit.
+    OPEN INPUT Fproduit
+    PERFORM WITH TEST AFTER UNTIL fp_idProduit > 0
+        DISPLAY "Saisir l'identifiant du produit : "
+        ACCEPT fp_idProduit
+    END-PERFORM
+    READ Fproduit
+    INVALID KEY MOVE 0 TO pe
+    NOT INVALID KEY MOVE 1 TO pe
+    END-READ
+    CLOSE Fproduit.
 
-            DISPLAY "Saisir le nom du produit : "
-            WRITE produitTemp.fp_nom
-
-            PERFORM WITH TEST AFTER UNTIL produitTemp.fp_prix > 0
-                DISPLAY "Saisir le prix du produit : "
-                WRITE produitTemp.fp_prix
-            END-PERFORM
-
-            PERFORM WITH TEST AFTER UNTIL produitTemp.fp_quantite > 0
-                DISPLAY "Saisir la quantite du produit : "
-                WRITE produitTemp.fp_quantite
-            END-PERFORM
-
-            WRITE produitTemp
-
-            CLOSE Fproduit.
+ajout_produit.
+        OPEN EXTEND Fproduit
+        PERFORM WITH TEST AFTER UNTIL fp_idProduit > 0
+        AND pe = 0
+            DISPLAY "Saisir l'identifiant du produit : "
+            ACCEPT fp_idProduit
+            PERFORM recherche_produit
+        END-PERFORM
+        DISPLAY "Saisir le nom du produit : "
+        ACCEPT fp_nom
+        PERFORM WITH TEST AFTER UNTIL fp_prix > 0
+            DISPLAY "Saisir le prix du produit : "
+            ACCEPT fp_prix
+        END-PERFORM
+        PERFORM WITH TEST AFTER UNTIL fp_quantite > 0
+            DISPLAY "Saisir la quantite du produit : "
+            ACCEPT fp_quantite
+        END-PERFORM
+        WRITE produitTemp
+        CLOSE Fproduit.
